fbd396e708c3edeaa58deda274ba090396cfa91c
JanweyGapate <janweygapate@gmail.com>
2024-10-30 23:01:14 +0800

    Create Coding Problem 1: User Information Collection.js
    1. File Creation:  Create a new JavaScript file named  userProfile.js .
    2. Variable Declaration: Declare variables to store the user's information.
    3. User Input: Use the  prompt()  function to collect user input for each piece of information.
    4. Storing Information:  Assign the user's input to the respective variables.
    5. Logging to Console: Log the collected user information to the console using  console.log() .

---
+++ Coding Problem 1.js
@@ -0,0 +1,12 @@
+
+// userProfile.js
+let firstName = prompt("Janwey:");
+let lastName = prompt("Gapate:");
+let age = prompt("19:");
+let favoriteNumber = prompt("22:");
+let favoriteColor = prompt("Brown:");
+
+console.log("Full Name:", firstName + " " + lastName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
