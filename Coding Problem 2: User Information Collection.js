7e0cd75f636987be8eb8ada5b89b40beffdd355a
JanweyGapate <janweygapate@gmail.com>
2024-10-31 00:23:27 +0800

    Create Coding Problem 2: User Information Collection.js
    1. - We declare variables using  let  to store the user input for each field:  subjectTitle ,  classSchedule ,  classroom ,  classInstructor , and  studentName .
    2.- We use the  prompt()  function to ask the user to enter the information. Each prompt message clearly indicates what information is needed.
    3.- We use template literals ( `` ) to create a formatted string that combines the user input with the desired sentence structure.
    4.- Finally, we use  console.log()  to display the formatted string in the browser's console, providing a visual representation of the collected information.

---
+++ Coding Problem 2: User Information Collection.js
@@ -0,0 +1,12 @@
+ 
+// classSubject.js
+
+// Collect user information
+let subjectTitle = prompt("Enter the subject title:");
+let classSchedule = prompt("Thursday  (12:00,31):");
+let classroom = prompt("Comlabc:");
+let classInstructor = prompt("Co Pascua:");
+let studentName = prompt("Gapate Janwey:");
+
+// Log the information to the console
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
